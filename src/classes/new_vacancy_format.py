fbeac99a05a20d5f620c2ff50cffb4ca7dd6c5ff
Katya Smirnova <vismanork@gmail.com>
2024-03-19 20:10:22 +0300

    Попытка правок класса NewVacancyFormat

--- src/classes/new_vacancy_format.py
+++ src/classes/new_vacancy_format.py
@@ -13,20 +13,48 @@ class NewVacancyFormat:
         for vacancy_data in vacancies_data:
             snippet = vacancy_data['snippet']
             salary = vacancy_data['salary']
+            print(salary)
             if salary:
                 salary_from = salary.get('from')
                 salary_to = salary.get('to')
-                salary_range = f"{salary_from}-{salary_to}" if salary_from and salary_to else None
-            else:
-                salary_range = "Данные отсутствуют"
+                if salary_from is not None:  # {'from': 10000, 'to': 60000}
+                    if salary_to is not None:  # {'from': 10000, 'to': 60000}
+                        salary_range = f'{salary_from}-{salary_to} руб.'
+                    else:  # {'from': 10000, 'to': null}
+                        salary_range = f'от {salary_from} руб.'
+                else:  # {'from': null, 'to': 60000}
+                    if salary_to is not None:  # {'from': null, 'to': 60000}
+                        salary_range = f'до {salary_to} руб.'
+                    else:  # {'from': null, 'to': null}
+                        salary_range = 'данные о заработной плате отсутствуют'
 
-            vacancy = Vacancy(
-                title=vacancy_data['name'],
-                url=vacancy_data['alternate_url'],
-                employer=vacancy_data['employer']['name'],
-                requirement=snippet.get('requirement', ''),
-                description=snippet.get('responsibility', ''),
-                salary=salary_range
-            )
-            vacancies.append(vacancy)
+            #     if salary_from is not None:  # {'from': 10000, 'to': 60000, 'currency': 'RUR', 'gross': True}
+            #         if salary_to is not None:  # {'from': 10000, 'to': 60000, 'currency': 'RUR', 'gross': True}
+            #             salary_range = f"{salary_from}-{salary_to}"
+            #         else:  # {'from': 10000, 'to': None, 'currency': 'RUR', 'gross': False}
+            #             salary_range = f"от {salary_from} руб."
+            #     else:  # {'from': None, 'to': 50000, 'currency': 'RUR', 'gross': True}
+            #         salary_range = f"до {salary_to} руб."
+            # else:
+            #     salary_range = salary_range_default
+
+            #     if salary_from is not None and salary_to is not None:
+            #         salary_range = f"{salary_from}-{salary_to}"
+            #     elif salary_from is not None:
+            #         salary_range = f"от {salary_from}"
+            #     elif salary_to is not None:
+            #         salary_range = f"до {salary_to}"
+            # else:
+            #     salary_range = salary_range_default
+
+                vacancy = Vacancy(
+                    title=vacancy_data['name'],
+                    url=vacancy_data['alternate_url'],
+                    employer=vacancy_data['employer']['name'],
+                    requirement=snippet.get('requirement', ''),
+                    description=snippet.get('responsibility', ''),
+                    salary=salary_range
+                )
+                vacancies.append(vacancy)
+                print(vacancy)
         return vacancies
